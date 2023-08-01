639d3e10621399a17310073526616f97c31a12f0
JoaoPrestupa <joao_prestupa11@hotmail.com>
2023-08-01 13:46:43 -0300

    implementar consultascontroller

--- src/main/java/br/com/clinicavt/controllers/ConsultaController.java
+++ src/main/java/br/com/clinicavt/controllers/ConsultaController.java
@@ -1,14 +1,43 @@
 package br.com.clinicavt.controllers;
 
+import br.com.clinicavt.dtos.ConsultaRecordDto;
+import br.com.clinicavt.models.ConsultaModel;
+import br.com.clinicavt.models.VeterinarioModel;
 import br.com.clinicavt.repositories.ConsultaRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
+@RequestMapping("/consultas")
 public class ConsultaController {
 
     @Autowired
     ConsultaRepository consultaRepository;
+
+    @GetMapping
+    public ResponseEntity<List<VeterinarioModel>> getAllConsultas(){
+
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getOneConsulta(@PathVariable ("id") UUID id){
+
+    }
+
+    @PostMapping("/marcar")
+    public ResponseEntity<ConsultaModel> saveConsulta(@RequestBody @Valid ConsultaRecordDto consultaDto){
+
+    }
+
+    @DeleteMapping("/desmarcar/{id}")
+    public ResponseEntity<Object> deleteConsulta(@PathVariable (value = "id") UUID id){
+
+    }
 
 
 }
